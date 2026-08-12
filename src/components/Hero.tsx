b7e7ba175365333037cd16b1e4c88d7aac9e2be7
RobertAve <saydonave017@gmail.com>
2026-08-12 17:02:40 +0800

    refactor: update styles and structure for responsive design across components

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,8 @@
-import Butterfly from './icons/Butterfly'
-import Silhouette from './icons/Silhouette'
-import RibbonDivider from './icons/RibbonDivider'
-import { eventDetails } from '../data/eventDetails'
-import './Hero.css'
+import Butterfly from "./icons/Butterfly";
+import Silhouette from "./icons/Silhouette";
+import RibbonDivider from "./icons/RibbonDivider";
+import { eventDetails } from "../data/eventDetails";
+import "./Hero.css";
 
 /** The opening section: name, occasion, date, time, venue, and a link to details. */
 export default function Hero() {
@@ -15,34 +15,36 @@ export default function Hero() {
         wingColorDeep="#DDA9B8"
       />
 
-      <p className="hero__eyebrow">You're invited to</p>
+      <div className="hero__inner">
+        <p className="hero__eyebrow">You're invited to</p>
 
-      <Silhouette className="hero__silhouette" />
+        <Silhouette className="hero__silhouette" />
 
-      <h1 className="hero__name">
-        {eventDetails.celebrantPossessive} {eventDetails.age}
-        <sup>th</sup>
-      </h1>
-      <div className="hero__occasion">{eventDetails.occasion}</div>
+        <h1 className="hero__name">
+          {eventDetails.celebrantPossessive} {eventDetails.age}
+          <sup>th</sup>
+        </h1>
+        <div className="hero__occasion">{eventDetails.occasion}</div>
 
-      <RibbonDivider className="hero__divider" />
+        <RibbonDivider className="hero__divider" />
 
-      <div className="hero__date-row">
-        <span className="hero__day">{eventDetails.day}</span>
-        <span className="hero__num">{eventDetails.date}</span>
-        <span className="hero__month">{eventDetails.month}</span>
+        <div className="hero__date-row">
+          <span className="hero__day">{eventDetails.day}</span>
+          <span className="hero__num">{eventDetails.date}</span>
+          <span className="hero__month">{eventDetails.month}</span>
+        </div>
+
+        <div className="hero__venue-block">
+          <p className="hero__venue-time">{eventDetails.time}</p>
+          <p className="hero__venue-place">{eventDetails.venue}</p>
+        </div>
+
+        <a href="#details" className="hero__cta">
+          See details
+        </a>
       </div>
-
-      <div className="hero__venue-block">
-        <p className="hero__venue-time">{eventDetails.time}</p>
-        <p className="hero__venue-place">{eventDetails.venue}</p>
-      </div>
-
-      <a href="#details" className="hero__cta">
-        See details
-      </a>
 
       <Butterfly className="hero__butterfly hero__butterfly--bottom-right" />
     </section>
-  )
+  );
 }
